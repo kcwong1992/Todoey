c24e69efc1f7bdbc8141421646360477ef255aa5
Kwan Choon Wong <kcwong@kwans-macbook-pro.local>
2019-02-12 22:46:26 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kwan Choon Wong on 2/12/19.
+//  Copyright © 2019 Kwan Choon Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
