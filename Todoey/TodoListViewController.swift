ca355d33f845208d12e63f6503e29985e228c8a7
Kwan Choon Wong <kcwong@kwans-macbook-pro.local>
2019-02-13 23:10:27 -0800

    Todo list complete with datasource and delegate method

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kwan Choon Wong on 2/12/19.
+//  Copyright © 2019 Kwan Choon Wong. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Dragon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark **To show check mark when row is selected
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) //**To unhighlight the row selected
+        
+        
+    }
+    
+    
+}
+
